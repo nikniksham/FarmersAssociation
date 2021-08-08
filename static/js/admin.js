8ecfc576b5f1767cb4b5d9059336dd13d07cf930
Rjkzavr <kolya.toropof@gmail.com>
2021-08-08 18:51:48 +0300

    make max count images

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,8 +1,10 @@
 var count_images = 1
-
+var max_count = 15
 
 
 function addImage(num) {
+    if count_images > max_count:
+        return
     let block = document.createElement('div');
     block.className = "input-file-row-1";
     block.innerHTML = '<div class="upload-file-container"><div class="delete-image"></div><img id="image'+count_images+'" class="this-is-image" src="#" alt=""/><input type="file" name="image'+count_images+'" class="photo" id="imgInput'+count_images+'"/></div>';
