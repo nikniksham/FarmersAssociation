5304bd303c31c75a7bcba5b5e6bc133dae6e0067
Rjkzavr <kolya.toropof@gmail.com>
2021-08-09 01:30:48 +0300

    any more fix and features

--- static/js/news.js
+++ static/js/news.js
@@ -1,8 +1,9 @@
 var loaded_news = 9
 var load_news = 9
+var link_website = "http://127.0.0.1:8000/"
 
 function add_news() {
-    $.get( "{{link_website}}/api/newspage/"+loaded_news+"/"+(loaded_news + load_news), function( data ) {
+    $.get( link_website+"api/newspage/"+loaded_news+"/"+(loaded_news + load_news), function( data ) {
         console.log(data);
         console.log(loaded_news+"/"+(loaded_news + load_news));
         loaded_news += load_news
