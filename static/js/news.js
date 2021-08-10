439478e545c8cd697b5d35706c85c66177ea8234
Niki <57631867+nikniksham@users.noreply.github.com>
2021-08-10 07:41:50 +0300

    На хероку

--- static/js/news.js
+++ static/js/news.js
@@ -1,6 +1,6 @@
 var loaded_news = 9
 var load_news = 9
-var link_website = "http://127.0.0.1:8000/"
+var link_website = ""
 
 function add_news() {
     $.get( link_website+"api/newspage/"+loaded_news+"/"+(loaded_news + load_news), function( data ) {
