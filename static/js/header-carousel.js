345f53195f8843b1cae96f323f85238223ebbe5e
Rjkzavr <kolya.toropof@gmail.com>
2021-08-15 23:53:48 +0300

    make text for head carousel

--- static/js/header-carousel.js
+++ static/js/header-carousel.js
@@ -16,13 +16,28 @@ $(document).ready(function() {
         pauseOnHover: false
     });
 
+    $('.header-carousel-text').slick({
+        accessibility: false,
+        speed: 500,
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: speedHeadCarousel - 500,
+        slidesToShow: 1,
+        draggable: false,
+        arrows: false,
+        fade: true,
+        cssEase: 'ease',
+        pauseOnHover: false
+    });
+
     $('.header-carousel').on('beforeChange', function(event, slick, currentSlide, nextSlide){
         console.log("change src");
         var images = $(".need-change");
         console.log(images);
+        $('.header-carousel').slick('slickGoTo', nextSlide);
         for (var i=0; i < images.length; i++) {
           $(images[i]).attr('src', $(images[i]).attr('src'));
         }
-        // $('.header-carousel').slick('slickGoTo', nextSlide);
     });
 });
